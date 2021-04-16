0b994e637b97f7f21afd0428bec05b7ffa06b060
Lio Young <lio@werewolf.design>
2021-04-16 15:44:50 +0200

    "dynamically" get route image amounts

--- v1/categories/index.ts
+++ v1/categories/index.ts
@@ -4,26 +4,22 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Image } from "@utils/api.types";
 import supabase from "@utils/database";
 import count from "@utils/count";
-
+import routes from "@utils/routes";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    let allRoutes: [] = []
+
+    routes.forEach((route) => {
+        allRoutes.push({
+            route: `/${route}`,
+            amount: count(route) || 0
+        })
+    })
+
 
     return res.json({
         success: true,
         message: "The valid routes are:",
-        routes: [
-            {
-                route: "/yeens",
-                amount: await count('yeens')
-            },
-            {
-                route: "/wolves",
-                amount: await count('wolves')
-            },
-            {
-                route: "/foxes",
-                amount: await count('foxes')
-            },
-
-        ]//.sort((a, b) => a.amount + b.amount)
+        routes: allRoutes
     })
 }
