3feddfddb8b21da0054086d9d55ca97179156e95
Lio Young <lio@werewolf.design>
2021-04-16 16:05:45 +0200

    move to cache

--- v1/categories/[category].ts
+++ v1/categories/[category].ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import FolderCategory from "@utils/category"
 import apikey from "@utils/apikey"
-import supabase from "@utils/database"
+
+
 import { Image } from "@utils/api.types"
+import cache from '@utils/cache'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { category } = req.query
@@ -15,8 +17,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (!authed.success) return res.json(authed)
     // @ts-ignore
-    let { data, error } = await supabase.from<Image>('images').select('*').eq("folder", folder)
-    if (!data) return res.json(false)
+    let data: Image[] = cache.get(category)
+    if (!data) {
+        // @ts-ignore
+        data = [{
+            id: 16918,
+            folder: "wolves",
+            file: "9b7938f053faef5e318e6168fffc9954",
+            ext: "jpg",
+            // @ts-ignore
+            notes: [
+                { id: 1, note: "Your are not supposed to see this Note, it will only show up when the cache of the API hasn't been called yet" },
+                { id: 2, note: "If you know how to have Next.js call something on startup, please send a PR in the thaldrin/temere repo" },
+            ]
+        }]
+    }
 
     let image = data[Math.floor(Math.random() * data.length)];
     // @ts-ignore
