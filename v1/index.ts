52489d52e3af718c68f37bd63e9c4dc9d13dbac6
Lio Young <lio@werewolf.design>
2021-04-16 03:46:17 +0200

    ignore req not being used

--- v1/index.ts
+++ v1/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// @ts-ignore
 export default (req: NextApiRequest, res: NextApiResponse) => {
     return res.json({
         success: true
