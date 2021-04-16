f569739b536ad692fc9e26796d59519a964caa47
Lio Young <lio@werewolf.design>
2021-04-16 17:32:49 +0200

    don't use cache for getting images

--- v1/categories/[category].ts
+++ v1/categories/[category].ts
@@ -17,20 +17,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (!authed.success) return res.json(authed)
     // @ts-ignore
-    let data: Image[] = cache.get(category)
+    let { data, error } = await supabase.from<Image>('images').select('*').eq("folder", folder)
     if (!data) {
-        // @ts-ignore
-        data = [{
-            id: 16918,
-            folder: "wolves",
-            file: "9b7938f053faef5e318e6168fffc9954",
-            ext: "jpg",
-            // @ts-ignore
-            notes: [
-                { id: 1, note: "Your are not supposed to see this Note, it will only show up when the cache of the API hasn't been called yet" },
-                { id: 2, note: "If you know how to have Next.js call something on startup, please send a PR in the thaldrin/temere repo" },
-            ]
-        }]
+        return res.json({
+            success: false,
+            message: "We did not find anything, but somehow you still got access ot this."
+        })
     }
 
     let image = data[Math.floor(Math.random() * data.length)];
