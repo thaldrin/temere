7e5b34a597bdee422bdfc5b41d33e1ed1824845d
Lio Young <lio@werewolf.design>
2021-04-17 20:49:19 +0200

    start with apikey generation

---
+++ admin/apikey.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// @ts-ignore
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    return res.json({
+        success: true
+    })
+}
+
+
+// TODO Generate API Keys
