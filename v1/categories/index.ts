e88fa3664ede0b3a45a609d66bf02db1c7e97ab4
Lio Young <lio@werewolf.design>
2021-04-17 14:31:54 +0200

    don't include route counts

--- v1/categories/index.ts
+++ v1/categories/index.ts
@@ -7,19 +7,11 @@ import count from "@utils/count";
 import routes from "@utils/routes";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
-    let allRoutes: [] = []
-
-    routes.forEach((route) => {
-        allRoutes.push({
-            route: `/${route}`,
-            amount: count(route) || 0
-        })
-    })
 
 
     return res.json({
         success: true,
         message: "The valid routes are:",
-        routes: allRoutes
+        routes
     })
 }
